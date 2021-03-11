21cb491ac7f812ee619864599fdca16d230fb148
Martin3 <strbanik.jun@gmail.com>
2021-03-11 10:34:31 +0100

    Started, test

---
+++ backend-kalceto/src/main/java/com/backend/backendkalceto/BackendKalcetoApplication.java
@@ -0,0 +1,14 @@
+package com.backend.backendkalceto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendKalcetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendKalcetoApplication.class, args);
+		System.out.println("adsfdsf");
+	}
+
+}
---
+++ backend-kalceto/src/test/java/com/backend/backendkalceto/BackendKalcetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.backendkalceto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendKalcetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
